b87cbf29c770b6c2bdf8ae400fc94c6dc4d46672
Kamil Dudek <dudek.kamil17@gmail.com>
2021-01-11 20:31:58 +0100

    feature: return dict instead of list

--- backend/api/airport_info.py
+++ backend/api/airport_info.py
@@ -24,24 +24,28 @@ def airport_info(ICAO):
     url = f'https://api.flightplandatabase.com/nav/airport/{ICAO}'
     r = requests.get(url=url)
     data = r.json()
-    airport_info_list = [data['name'], data['lat'], data['lon'],
-                         data['elevation'], data['magneticVariation']]
-
+    airport_info_dict = {
+        'name': data['name'],
+        'latitude': data['lat'],
+        'longitude': data['lon'],
+        'elevation': data['elevation'],
+        'magnetic Varation': data['magneticVariation']
+    }
     runways_count = data['runwayCount'] * 2
+
     if runways_count == 2:
-
-        airport_info_list.append(data['runways'][0]['ident'])
-        airport_info_list.append(data['runways'][0]['navaids'][0]['lat'])
-        airport_info_list.append(data['runways'][0]['navaids'][0]['lon'])
-        airport_info_list.append(data['runways'][0]['surface'])
+        airport_info_dict['runway ident'] = data['runways'][0]['ident']
+        airport_info_dict['runway lat'] = data['runways'][0]['ends'][0]['lat']
+        airport_info_dict['runway lon'] = data['runways'][0]['ends'][0]['lon']
+        airport_info_dict['runway surface'] = data['runways'][0]['surface']
     else:
-
         for i in range(0, runways_count):
-            airport_info_list.append(data['runways'][i]['ident'])
-            airport_info_list.append(data['runways'][i]['ends'][0]['lat'])
-            airport_info_list.append(data['runways'][i]['ends'][0]['lon'])
-            airport_info_list.append(data['runways'][i]['surface'])
-    return json.dumps(airport_info_list)
+            airport_info_dict[f'runway ident{i}'] = data['runways'][i]['ident']
+            airport_info_dict[f'runway lat{i}'] = data['runways'][i]['ends'][0]['lat']
+            airport_info_dict[f'runway lon{i}'] = data['runways'][i]['ends'][0]['lon']
+            airport_info_dict[f'runway surface{i}'] = data['runways'][i]['surface']
+            
+    return airport_info_dict 
 
 
 @FLIGHT.route('/<string:ICAO>', methods=['GET'])
